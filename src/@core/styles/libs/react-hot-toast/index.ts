b2293323caa32f0ae5d4707106f632637e84d5a3
William Neves <drwilliamneves@gmail.com>
2022-09-28 17:56:26 -0400

    Toast with dark background on light theme.

--- src/@core/styles/libs/react-hot-toast/index.ts
+++ src/@core/styles/libs/react-hot-toast/index.ts
@@ -20,13 +20,15 @@ const ReactHotToast = styled(Box)<BoxProps>(({ theme }) => {
     '& .react-hot-toast': {
       fontWeight: 400,
       borderRadius: 8,
-      fontSize: '1rem',
-      letterSpacing: '0.14px',
+      fontSize: "1rem",
+      letterSpacing: "0.14px",
       boxShadow: theme.shadows[3],
       zIndex: theme.zIndex.snackbar,
-      color: theme.palette.text.primary,
-      background: theme.palette.background.paper,
-      '&>:first-of-type:not([role])>:first-of-type': {
+      //@ts-ignore
+      color: theme.toaster.color,
+      //@ts-ignore
+      background: theme.toaster.bg,
+      "&>:first-of-type:not([role])>:first-of-type": {
         width: 14,
         height: 14
       }
