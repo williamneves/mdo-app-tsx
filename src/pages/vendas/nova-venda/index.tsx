b396692e895693359962205d82b53c8ec29b5d6d
gwilliamnn <97652695+gwilliamnn@users.noreply.github.com>
2022-09-30 09:51:39 -0400

    Remove some unsusable imports

--- src/pages/vendas/nova-venda/index.tsx
+++ src/pages/vendas/nova-venda/index.tsx
@@ -1,9 +1,6 @@
 // ** MUI Imports
-import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
 
 // ** Component Imports
 import NovaVendaWizard from '@views/pages/vendas/nova-venda/NovaVendaWizard'
