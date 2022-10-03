73f87181c2b1bfaf4e5d1aa3db2d8e7dbfdfcc70
wendellmoraisz <wendellmorais3319@gmail.com>
2022-10-03 10:05:03 -0300

    feat: Street Daily Report schema

---
+++ server/schemas/StreetDailyReport.js
@@ -0,0 +1,74 @@
+export default {
+    title: "Relatório Diário do Street",
+    name: "streetDailyReport",
+    type: "document",
+    fields: [
+        {
+            title: "Inativo",
+            name: "inactive",
+            type: "boolean",
+        },
+        {
+            title: "Excluído",
+            name: "excluded",
+            type: "boolean",
+        },
+        {
+            title: "Data do Relatório",
+            name: "reportDate",
+            type: "date",
+            options: {
+                dateFormat: "DD/MM/YY",
+            },
+        },
+        {
+            title: "Reportado por",
+            name: "reporter",
+            type: "reference",
+            to: [{type: "user"}],
+        },
+        {
+            title: "Status de Auditoria",
+            name: "auditStatus",
+            type: "string",
+            options: {
+                list: [
+                    {title: "Pendente", value: "pending"},
+                    {title: "Aprovado", value: "approved"},
+                    {title: "Rejeitado", value: "rejected"},
+                ],
+                default: "pending",
+            },
+        },
+        {
+            title: "Clientes Abordados",
+            name: "clientsApproached",
+            type: "number",
+        },
+        {
+            title: "Clientes Cadastrados",
+            name: "clientsRegistered",
+            type: "array",
+            of: [{
+                type: "reference",
+                to: [{type: "client"}],
+            }],
+        },
+        {
+            title: "Consultas Agendadas",
+            name: "scheduledAppointments",
+            type: "number",
+        },
+        {
+            title: "Relatório de Atividades",
+            name: "activitiesReport",
+            type: "string",
+        },
+        {
+            title: "Store",
+            name: "store",
+            type: "reference",
+            to: [{type: "store"}],
+        },
+    ]
+}
