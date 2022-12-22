0b8193132ac85e0d6dd63fed1fb4d3070aa10217
wendellmoraisz <wendellmorais3319@gmail.com>
2022-12-21 21:28:25 -0300

    feat: manager report schema

---
+++ server/schemas/managerReport.js
@@ -0,0 +1,60 @@
+export default {
+    title: "Manager Report",
+    name: "managerReport",
+    type: "document",
+    preview: {
+        select: {
+            title: "date",
+            subtitle: "valueInCash",
+        }
+    },
+    fields: [
+        {
+            title: "Valor em caixa",
+            name: "valueInCash",
+            type: "number",
+        },
+        {
+            title: "Data",
+            name: "date",
+            type: "date",
+            options: {
+                dateFormat: "DD/MM/YY",
+            }
+        },
+        {
+            title: "Decrição do dia",
+            name: "dayDescription",
+            type: "string",
+        },
+        {
+            title: "Planejamento para o dia seguinte",
+            name: "nextDayPlanning",
+            type: "string",
+        },
+        {
+            title: "Vendas Aprovadas",
+            name: "approvedSales",
+            type: "array",
+            of: [{
+                type: "reference",
+                to: [{type: "sale"}],
+            }],
+        },
+        {
+            title: "Anexos",
+            name: "docs",
+            type: "",
+        },
+        {
+            title: "Consultas Marcadas",
+            name: "scheduledAppointments",
+            type: "number",
+        },
+        {
+            title: "Consultas Realizadas",
+            name: "consultationsMade",
+            type: "number",
+        },
+    ],
+}
